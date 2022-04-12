07cdf411e5df7f404ff081c7c2c0560694895d17
vini.desconsi <vdesconsi@gmail.com>
2022-04-12 19:37:38 -0300

    add try catch na service

--- lib/app/screens/home/home_service.dart
+++ lib/app/screens/home/home_service.dart
@@ -8,31 +8,45 @@ import 'package:crypto/crypto.dart';
 class HomeService {
   //metodo que busca na api
   Future<CharactersModel?> getCharacters(int offset) async {
-    var characters = CharactersModel();
-    var dio = Dio();
-    var _publicKey = '5baf9b7eda23eae943822628589ca81b';
-    var _privateKey = '9e9dd24afd86b25040a8b88dab6028b86627355e';
-    var timeStamp = DateTime.now().millisecondsSinceEpoch;
-    var text = timeStamp.toString() + _privateKey + _publicKey;
-    var crypt = md5.convert(utf8.encode(text)).toString();
+    try {
+      var characters = CharactersModel();
+      var dio = Dio();
+      var _publicKey = '5baf9b7eda23eae943822628589ca81b';
+      var _privateKey = '9e9dd24afd86b25040a8b88dab6028b86627355e';
+      var timeStamp = DateTime.now().millisecondsSinceEpoch;
+      var text = timeStamp.toString() + _privateKey + _publicKey;
+      var crypt = md5.convert(utf8.encode(text)).toString();
 
-    var response = await dio.get(
-      'http://gateway.marvel.com/v1/public/characters?offset=$offset&ts=$timeStamp&apikey=$_publicKey&hash=$crypt',
-    );
+      var response = await dio.get(
+        'http://gateway.marvel.com/v1/public/characters?offset=$offset&ts=$timeStamp&apikey=$_publicKey&hash=$crypt',
+      );
 
-    if (response.statusCode == 200) {
-      characters = CharactersModel.fromJson(response.data);
-    } else {
+      if (response.statusCode == 200) {
+        characters = CharactersModel.fromJson(response.data);
+      } else {
+        Get.snackbar(
+          "",
+          "",
+          snackPosition: SnackPosition.BOTTOM,
+          titleText: Center(
+            child: Text("${response.statusCode} - Falha ao buscar dados"),
+          ),
+        );
+      }
+
+      return characters;
+    } catch (e) {
+      print(e.toString());
       Get.snackbar(
         "",
         "",
         snackPosition: SnackPosition.BOTTOM,
         titleText: Center(
-          child: Text("${response.statusCode} - Falha ao buscar dados"),
+          child: Text(
+            e.toString(),
+          ),
         ),
       );
     }
-
-    return characters;
   }
 }
